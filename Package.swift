694656b64073303f5e0ac6c2992dc54b0a57f6e3
The iOS Team @ GeneXus <builder@genexus.com>
2024-12-10 05:46:56 -0300

    Package version: 2.1.4

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXCoreModule_Common_MapsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_GeoLocation.git", exact: "2.1.3"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.1.3")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_GeoLocation.git", exact: "2.1.4"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.1.4")
 	],
 	targets: [
 		.target(name: "GXCoreModule_Common_MapsWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_Common_Maps",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_Common_Maps-2.1.3.xcframework.zip",
-			checksum: "9fe77e643311b93665a11627483f97b502052af92f195dedb92c13058cdeeff4"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_Common_Maps-2.1.4.xcframework.zip",
+			checksum: "30e04cfd74f24b779bc999430737846b976842894907416512c25f9674e4abb7"
 		)
 	]
 )
